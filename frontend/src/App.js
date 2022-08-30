f44c6f8cdfe34d9ab1711293b02224b89574970e
Bryan Navarrete <bryanhenrry94@gmail.com>
2022-08-30 18:50:50 -0500

    se eliminaron caracteres especiales en App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,10 @@
-<<<<<<< HEAD
 import { useState } from 'react'
 import './App.css'
 import qs from 'qs'
 import axios from 'axios'
-=======
 import React from 'react'
 import Login from '@pages/Login'
 import LoginDemo from '@pages/LoginDemo'
->>>>>>> 7d9f4e669c00a72ed787b9fba5a7c90923c0d0ab
 
 function App () {
   return (
