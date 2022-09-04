2e6d4e9c227c6fcd3f4ba402e88566bcec8ff0d2
John Navarrete <jwnavarretez@gmail.com>
2022-09-04 01:48:49 -0500

    footer modificado

--- frontend/src/layout/MainLayout/Footer/components.js
+++ frontend/src/layout/MainLayout/Footer/components.js
@@ -2,10 +2,39 @@ import styled from "styled-components";
 
 export const Footer = styled.footer`
   grid-area: footer;
-  margin: 0px 15px 5px 15px;
-  background-color: #ffffff;
-  /* BBORDER CON SOMBRA */
-  box-shadow: -1px 1px 7px 0px rgba(0, 0, 0, 0.25);
-  border-radius: 4px;
-  text-align: center;
+  padding: 0.8rem 2rem;
+  min-height: 3.35rem;
+  display: block;
+`;
+
+export const Paragraph = styled.p`
+  margin-bottom: 0 !important;
+  margin-top: 0;
+  margin-bottom: 1rem;
+  display: block;
+  /* margin-block-start: 1em;
+  margin-block-end: 1em; */
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+`;
+
+export const SpanLeft = styled.span`
+  margin-top: 0.25rem !important;
+  float: left !important;
+  font-size: 14px;
+  line-height: 1.5rem;
+`;
+
+export const LinkSite = styled.a`
+  margin-left: 0.25rem !important;
+  background-color: transparent;
+  color: red;
+  text-decoration: none;
+`;
+
+export const SpanRight = styled.span`
+  display: block!important;
+  float: right !important;
+  font-size: 14px;
+  line-height: 1.5rem;
 `;
