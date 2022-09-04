40ea1f9bea58a2fed43d5bd1f861936eedf6614e
Bryan Navarrete <bryanhenrry94@gmail.com>
2022-09-04 00:06:38 -0500

    maquetacion del header

--- frontend/src/layout/MainLayout/Header/components.js
+++ frontend/src/layout/MainLayout/Header/components.js
@@ -1,7 +1,41 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const Header = styled.header`
   grid-area: header;
   background-color: #ffffff;
   margin: 15px 15px 0px 15px;
-`;
+  display: flex;
+  flex-direction: row;
+`
+
+export const Navbar = styled.nav`
+  width: 100%;
+`
+
+export const Box = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+`
+
+export const Ul = styled.ul`
+  display: flex;
+  flex-direction: row;
+  list-style: none;
+  align-items: center;
+`
+
+export const Li = styled.li`
+  padding: 15px;
+`
+
+export const Anchor = styled.a`
+  width: 50px;
+  text-decoration: none;
+`
+
+export const Row = styled.div`
+  flex: 1;
+`
